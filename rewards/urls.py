3ba41d6d61fbf06d28db56b0fe104608425bd052
lubabah5 <lubabahalghoshwork@gmail.com>
2025-02-02 21:35:57 +0300

    Add many update

--- rewards/urls.py
+++ rewards/urls.py
@@ -22,3 +22,21 @@ urlpatterns = [
     path('rewards/history/<str:user_id>/', UserRewardHistoryView.as_view(), name='user-reward-history'),
 
 ]
+# http://127.0.0.1:8000/rewards/rewards/create/ إنشاء جائزة
+# {
+#     "name": "Daily Login",
+#     "description": "Reward for logging in daily.",
+#     "points_required": 30
+# }
+
+#http://127.0.0.1:8000/rewards/rewards/grant/   ارسال عدد النقاط والتأكد من حصوله على جائزة
+# {
+#     "user_id": "676313e71c6fa6b40d339c6a",
+#     "total_points": 30
+# }
+
+#http://127.0.0.1:8000/rewards/rewards/history/676313e71c6fa6b40d339c6a/ استرجاع كل جوائز مستخدم معين
+
+#http://127.0.0.1:8000/rewards/rewards/list/ استرجاع كل الجوائز
+
+#http://127.0.0.1:8000/rewards/rewards/6798d84e78a68cb3d2bee4b2/delete/ حذف جائزة
