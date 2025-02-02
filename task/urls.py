e9daf0cfa8bd789272fd0d8bcd89b48f42450d3d
lubabah5 <lubabahalghoshwork@gmail.com>
2025-02-02 21:45:35 +0300

    Add many comment

--- task/urls.py
+++ task/urls.py
@@ -42,10 +42,9 @@ urlpatterns = [
     path('template-tasks/update/', UpdateTemplateTaskView.as_view(), name='update_template_task'),
     #نقل التاسكات من جدول تاسكات القوالب الى جدول التاسكات الكلي
     path('assign-template-tasks/<str:template_id>/', AssignTemplateTasksView.as_view(), name='assign-template-tasks'),
-
 ]
 
-#  إضافة مهمة جديدة           http://127.0.0.1:8000/api/tasks/tasks/
+#  إضافة مهمة جديدة           http://127.0.0.1:8000/tasks/tasks/
 # {
 #   "UserID": "user123",
 #   "Title": "Complete Homework",
@@ -57,21 +56,21 @@ urlpatterns = [
 #   "Status": "Pending"
 # }
 
-# http://127.0.0.1:8000/api/tasks/tasks/67869f5351973cc0e6d796a7/ استرجاع مهمة ما عن طريق id المهمة
-# http://localhost:8000/api/tasks/tasks/user/64ae23f5cd6d4a1234567890/  استرجاع مهمة ما عن طريق id المستخدم
-# http://127.0.0.1:8000/api/tasks/tasks/delete/6786a881ef3345ac72d5a201/ حذف مهمة عن طريق id المهمة
-# http://127.0.0.1:8000/api/tasks/tasks/update/6787db08e744c291dc9b199b/ التعديل على مهمة عن طريق id المهمة
-# http://127.0.0.1:8000/api/tasks/tasks/ استرجاع جميع المهام من قاعدة البيانات
+# http://127.0.0.1:8000/tasks/tasks/67869f5351973cc0e6d796a7/ استرجاع مهمة ما عن طريق id المهمة
+# http://localhost:8000/tasks/tasks/user/64ae23f5cd6d4a1234567890/  استرجاع مهمة ما عن طريق id المستخدم
+# http://127.0.0.1:8000/tasks/tasks/delete/6786a881ef3345ac72d5a201/ حذف مهمة عن طريق id المهمة
+# http://127.0.0.1:8000/tasks/tasks/update/6787db08e744c291dc9b199b/ التعديل على مهمة عن طريق id المهمة
+# http://127.0.0.1:8000/tasks/tasks/ استرجاع جميع المهام من قاعدة البيانات
 
 
 
 ####################القوالب ###################################
-# http://127.0.0.1:8000/api/tasks/templates/ إضافة قالب وعرض القوالب الموجودة
-# http://127.0.0.1:8000/api/tasks/delete/template/name/Normal Day/ حذف قالب باستخدام الاسم
-# http://127.0.0.1:8000/api/tasks/delete/template/id/6783098111977c36a859d7d3/ حذف القالب باستخدام id القالب
+# http://127.0.0.1:8000/tasks/templates/ إضافة قالب وعرض القوالب الموجودة
+# http://127.0.0.1:8000/tasks/delete/template/name/Normal Day/ حذف قالب باستخدام الاسم
+# http://127.0.0.1:8000/tasks/delete/template/id/6783098111977c36a859d7d3/ حذف القالب باستخدام id القالب
 
 
-#http://127.0.0.1:8000/api/tasks/template-tasks/add/ إضافة مهمة الى تاسك معين مع الانتباه الى ان المستخدم يجب ان يكون ادمن 
+#http://127.0.0.1:8000/tasks/template-tasks/add/ إضافة مهمة الى تاسك معين مع الانتباه الى ان المستخدم يجب ان يكون ادمن 
 # {
 #     "TemplateID": "6788097711977c36a859d7d2",
 #     "TaskID": "task456",
@@ -84,13 +83,13 @@ urlpatterns = [
 #     "Repetition": "Daily"
 # }
 
-#http://127.0.0.1:8000/api/tasks/template-tasks/delete/ حذف تاسك خاص بالقالب بحيث نعطي الbodey معلومات id القالب و _id المهمة
+#http://127.0.0.1:8000/tasks/template-tasks/delete/ حذف تاسك خاص بالقالب بحيث نعطي الbodey معلومات id القالب و _id المهمة
 #مثال
 # {
 #   "TemplateID": "6788097711977c36a859d7d2",
 #   "TaskID": "678a8faee4978149c8a26f8b"
 # }
-#http://127.0.0.1:8000/api/tasks/template-tasks/update/ التعديل على مهام القالب
+#http://127.0.0.1:8000/tasks/template-tasks/update/ التعديل على مهام القالب
 #{
 #     "TemplateID": "67880a49670aa7facb59617a",
 #     "_id": "678ab031fe13894c62f1b19b",
@@ -101,7 +100,7 @@ urlpatterns = [
 #         "Point": 15.0
 #     }
 # }
-#http://127.0.0.1:8000/api/tasks/assign-template-tasks/67880a49670aa7facb59617a/ نقل التاسكات التي اختارها المستخدم من جدول تاسكات القوالب الى جدول التاسكات الكلي مع اعطاءه id المستخدم
+#http://127.0.0.1:8000/tasks/assign-template-tasks/67880a49670aa7facb59617a/ نقل التاسكات التي اختارها المستخدم من جدول تاسكات القوالب الى جدول التاسكات الكلي مع اعطاءه id المستخدم
 #{
 #     "user_id": "676313e71c6fa6b40d339c6a"
 # }
